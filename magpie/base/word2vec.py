bcb9a0c80edcd84427269759a721255036f96b41
Jasmin Rubinovitz <jasrub@gmail.com>
2017-02-24 10:26:09 -0500

    fix requirements

--- magpie/base/word2vec.py
+++ magpie/base/word2vec.py
@@ -2,14 +2,6 @@ import os
 
 import numpy as np
 
-from gensim.models import Word2Vec
-from sklearn.preprocessing import StandardScaler
-
-from magpie.base.document import Document
-from magpie.config import WORD2VEC_MODELPATH, EMBEDDING_SIZE
-from magpie.utils import get_documents, save_to_disk, get_documents_from_mongo
-
-
 
 def compute_word2vec_for_phrase(phrase, model):
     """
