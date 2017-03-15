cf04c9b6548b62737d3661f4765c5f969c838925
Rahul B <rahulbot@gmail.com>
2017-03-15 16:16:26 -0400

    add, and use, json formatted endpoint

--- app.py
+++ app.py
@@ -18,20 +18,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict.json', methods=['POST'])
 def predict():
-    logger.debug("Request to predict")
+    logger.debug("Request to predict.json")
     text = request.json["text"]
     res600 = models.model600.predict(text)
     res3000 = models.model3000.predict(text)
     res_all = models.model_all.predict(text)
     res_with_tax = models.model_with_tax.predict(text)
     res_just_tax = models.model_just_tax.predict(text)
-    return jsonify({'descriptors_600': "\n".join(["%s : %s" % (x[0], "{0:.5f}".format(x[1])) for x in res600[:30]]),
-                    'descriptors_3000': "\n".join(["%s : %s" % (x[0], "{0:.5f}".format(x[1])) for x in res3000[:30]]),
+    return jsonify({'descriptors600': [{'label': x[0], 'score': "{0:.5f}".format(x[1])} for x in res600[:30]],
+                    'descriptors3000': [{'label': x[0], 'score': "{0:.5f}".format(x[1])} for x in res3000[:30]],
+                    'allDescriptors': [{'label': x[0], 'score': "{0:.5f}".format(x[1])} for x in res_all[:30]],
+                    'descriptorsAndTaxonomies': [{'label': x[0], 'score': "{0:.5f}".format(x[1])} for x in res_with_tax[:30]],
+                    'taxonomies': [{'label': x[0], 'score': "{0:.5f}".format(x[1])} for x in res_just_tax[:30]],
+                    })
+
+
+@app.route('/predict', methods=['POST'])
+def dcgan():
+    text = request.json["text"]
+    res600 = models.model600.predict(text)
+    res3000 = models.model3000.predict(text)
+    res_all = models.model_all.predict(text)
+    res_with_tax = models.model_with_tax.predict(text)
+    res_just_tax = models.model_just_tax.predict(text)
+    return jsonify({'descriptors_600': "\n".join(["%s : %s"%(x[0], "{0:.5f}".format(x[1])) for x in res600[:30]]),
+                    'descriptors_3000': "\n".join(["%s : %s"%(x[0], "{0:.5f}".format(x[1])) for x in res3000[:30]]),
                     'all_descriptors': "\n".join(["%s : %s" % (x[0], "{0:.5f}".format(x[1])) for x in res_all[:30]]),
                     'descriptors_and_tax': "\n".join(["%s : %s" % (x[0], "{0:.5f}".format(x[1])) for x in res_with_tax[:30]]),
                     'taxonomies': "\n".join(["%s : %s" % (x[0], "{0:.5f}".format(x[1])) for x in res_just_tax[:30]]),
                     })
-
-logger.info("  created web app")
